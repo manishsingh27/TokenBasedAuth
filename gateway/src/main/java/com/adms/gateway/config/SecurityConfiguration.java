ee075ff5685431948bd3457f1c3d30d5295201f6
manis <manis@DESKTOP-KAMR1QE>
2017-03-29 23:35:19 -0400

    Gateway initial commit

---
+++ gateway/src/main/java/com/adms/gateway/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.adms.gateway.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableOAuth2Sso
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.logout().and()
+			.authorizeRequests()
+				.antMatchers("/index.html", "/home.html", "/", "/login").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+			// @formatter:on
+	}
+
+}
